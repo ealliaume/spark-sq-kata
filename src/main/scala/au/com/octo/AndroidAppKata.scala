44021bc68c7d53f69fadd94a9535a27d7ec23a62
Erwan Alliaume <ealliaume@octo.com>
2018-10-12 14:49:49 +1100

    add solution

--- src/main/scala/au/com/octo/AndroidAppKata.scala
+++ src/main/scala/au/com/octo/AndroidAppKata.scala
@@ -6,23 +6,38 @@ import org.apache.spark.sql.{Dataset, Row, SparkSession}
 object AndroidAppKata {
 
   def step1LoadDataset(sparkSession: SparkSession, csvPath: String): Dataset[Row] = {
-    // FIX-ME use the spark session to read the csv
-    ???
+    sparkSession
+      .read
+      .option("header", "true")
+      .option("escape", "\"")
+      .csv(csvPath)
   }
 
   def step2KeepOnlyCategoryEducation(dataset: Dataset[Row]): Dataset[Row] = {
-    // FIX-ME filter the dataset to keep only the education EDUCATION
-    ???
+    dataset
+      .filter(col("Category") === "EDUCATION")
   }
 
   def step3MostReviewedApp(dataset: Dataset[Row]): Row = {
-    // FIX-ME select good columns and order by number or reviews desc
-    ???
+    dataset
+      .select(col("App"), col("Reviews").cast("int"))
+      .orderBy(desc("Reviews"))
+      .head()
   }
 
   def step4NumberOfPositiveReviewsApp(apps: Dataset[Row], reviews: Dataset[Row]): Dataset[Row] = {
-    // FIX-ME count the number of Positive review by App, then join with the main dataset
-    ???
+    reviews
+      .filter(col("Sentiment") === "Positive")
+
+      .select(col("App"), lit("1").as("value"))
+      .groupBy(col("App"))
+      .agg(sum(col("value")).as("nb_positives"))
+
+      .join(apps, "App")
+      .select("App", "Category", "Rating", "nb_positives")
+      .orderBy(desc("nb_positives"))
+      .dropDuplicates()
   }
+
 
 }
